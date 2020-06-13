307ecfdd5408b062e06fd2dec7f80ff5654268be
Helder Pinto <hepint@microsoft.com>
2020-06-13 17:38:53 +0100

    Merged RunAsAccount and added support for SAS token

--- Deploy-AzureOptimizationEngine.ps1
+++ Deploy-AzureOptimizationEngine.ps1
@@ -1,9 +1,108 @@
 param (
     [Parameter(Mandatory = $false)]
-    [string] $TemplateUri = "https://raw.githubusercontent.com/helderpinto/AzureOptimizationEngine/master/azuredeploy.json"
+    [string] $TemplateUri,
+
+    [Parameter(Mandatory = $false)]
+    [string] $AzureEnvironment = "AzureCloud",
+
+    [Parameter(Mandatory = $false)]
+    [string] $ArtifactsSasToken
 )
 
+function CreateSelfSignedCertificate([string] $certificateName, [string] $selfSignedCertPlainPassword,
+    [string] $certPath, [string] $certPathCer, [string] $selfSignedCertNoOfMonthsUntilExpired ) {
+    $Cert = New-SelfSignedCertificate -DnsName $certificateName -CertStoreLocation cert:\LocalMachine\My `
+        -KeyExportPolicy Exportable -Provider "Microsoft Enhanced RSA and AES Cryptographic Provider" `
+        -NotAfter (Get-Date).AddMonths($selfSignedCertNoOfMonthsUntilExpired) -HashAlgorithm SHA256
+
+    $CertPassword = ConvertTo-SecureString $selfSignedCertPlainPassword -AsPlainText -Force
+    Export-PfxCertificate -Cert ("Cert:\localmachine\my\" + $Cert.Thumbprint) -FilePath $certPath -Password $CertPassword -Force | Write-Verbose
+    Export-Certificate -Cert ("Cert:\localmachine\my\" + $Cert.Thumbprint) -FilePath $certPathCer -Type CERT | Write-Verbose
+}
+
+function CreateServicePrincipal([System.Security.Cryptography.X509Certificates.X509Certificate2] $PfxCert, [string] $applicationDisplayName) {
+    $keyValue = [System.Convert]::ToBase64String($PfxCert.GetRawCertData())
+    $keyId = (New-Guid).Guid
+
+    # Create an Azure AD application, AD App Credential, AD ServicePrincipal
+
+    # Requires Application Developer Role, but works with Application administrator or GLOBAL ADMIN
+    $Application = New-AzADApplication -DisplayName $ApplicationDisplayName -HomePage ("http://" + $applicationDisplayName) -IdentifierUris ("http://" + $keyId)
+    # Requires Application administrator or GLOBAL ADMIN
+    $ApplicationCredential = New-AzADAppCredential -ApplicationId $Application.ApplicationId -CertValue $keyValue -StartDate $PfxCert.NotBefore -EndDate $PfxCert.NotAfter
+    # Requires Application administrator or GLOBAL ADMIN
+    $ServicePrincipal = New-AzADServicePrincipal -ApplicationId $Application.ApplicationId
+    $GetServicePrincipal = Get-AzADServicePrincipal -ObjectId $ServicePrincipal.Id
+
+    # Sleep here for a few seconds to allow the service principal application to become active (ordinarily takes a few seconds)
+    Start-Sleep -Seconds 15
+    # Requires User Access Administrator or Owner.
+    $NewRole = New-AzRoleAssignment -RoleDefinitionName Contributor -ServicePrincipalName $Application.ApplicationId -ErrorAction SilentlyContinue
+    $Retries = 0;
+    While ($null -eq $NewRole -and $Retries -le 6) {
+        Start-Sleep -Seconds 10
+        New-AzRoleAssignment -RoleDefinitionName Contributor -ServicePrincipalName $Application.ApplicationId -ErrorAction SilentlyContinue
+        $NewRole = Get-AzRoleAssignment -ServicePrincipalName $Application.ApplicationId -ErrorAction SilentlyContinue
+        $Retries++;
+    }
+    return $Application.ApplicationId.ToString();
+}
+
+function CreateAutomationCertificateAsset ([string] $resourceGroup, [string] $automationAccountName, [string] $certifcateAssetName, [string] $certPath, [string] $certPlainPassword, [Boolean] $Exportable) {
+    $CertPassword = ConvertTo-SecureString $certPlainPassword -AsPlainText -Force
+    Remove-AzAutomationCertificate -ResourceGroupName $resourceGroup -AutomationAccountName $automationAccountName -Name $certifcateAssetName -ErrorAction SilentlyContinue
+    New-AzAutomationCertificate -ResourceGroupName $resourceGroup -AutomationAccountName $automationAccountName -Path $certPath -Name $certifcateAssetName -Password $CertPassword -Exportable:$Exportable  | write-verbose
+}
+
+function CreateAutomationConnectionAsset ([string] $resourceGroup, [string] $automationAccountName, [string] $connectionAssetName, [string] $connectionTypeName, [System.Collections.Hashtable] $connectionFieldValues ) {
+    Remove-AzAutomationConnection -ResourceGroupName $resourceGroup -AutomationAccountName $automationAccountName -Name $connectionAssetName -Force -ErrorAction SilentlyContinue
+    New-AzAutomationConnection -ResourceGroupName $ResourceGroup -AutomationAccountName $automationAccountName -Name $connectionAssetName -ConnectionTypeName $connectionTypeName -ConnectionFieldValues $connectionFieldValues
+}
+
+
 $ErrorActionPreference = "Stop"
+
+$GitHubOriginalUri = "https://raw.githubusercontent.com/helderpinto/AzureOptimizationEngine/master/azuredeploy.json"
+
+if ([string]::IsNullOrEmpty($TemplateUri))
+{
+    $TemplateUri = $GitHubOriginalUri
+}
+
+$isTemplateAvailable = $false
+
+if ($TemplateUri -ne $GitHubOriginalUri)
+{
+    try
+    {
+        Invoke-WebRequest -Uri $TemplateUri | Out-Null
+        $isTemplateAvailable = $true
+    }
+    catch
+    {
+        $saNameEnd = $TemplateUri.IndexOf(".blob.core.")
+        if ($saNameEnd -gt 0)
+        {
+            $FullTemplateUri = $TemplateUri + $ArtifactsSasToken
+            try {
+                Invoke-WebRequest -Uri $FullTemplateUri | Out-Null
+                $isTemplateAvailable = $true
+                $TemplateUri = $FullTemplateUri
+            }
+            catch {
+                Write-Host "The template URL ($TemplateUri) is not available. Please, provide a valid SAS Token in the ArtifactsSasToken parameter (Read permission and Object level access are sufficient)." -ForegroundColor Red
+            }
+        }
+        else {
+            Write-Host "The template URL ($TemplateUri) is not available. Please, put it in a publicly accessible HTTPS location." -ForegroundColor Red
+        }
+    }
+}
+
+if (!$isTemplateAvailable)
+{
+    throw "Terminating due to template unavailability."
+}
 
 $deploymentNameTemplate = "{0}" + (Get-Date).ToString("yyMMddHHmmss")
 $resourceGroupNameTemplate = "{0}-rg"
@@ -14,7 +113,7 @@ $sqlServerNameTemplate = "{0}-sql"
 
 $ctx = Get-AzContext
 if (-not($ctx)) {
-    Connect-AzAccount
+    Connect-AzAccount -Environment $AzureEnvironment
     $ctx = Get-AzContext
 }
 
@@ -128,7 +227,7 @@ if ("Y", "y" -contains $workspaceReuse) {
 }
 
 Write-Host "Getting Azure locations..." -ForegroundColor Green
-$locations = Get-AzLocation | Sort-Object -Property location
+$locations = Get-AzLocation | Where-Object {$_.Providers -contains "Microsoft.Automation"} | Sort-Object -Property Location
 
 for ($i = 0; $i -lt $locations.Count; $i++) {
     Write-Output "[$i] $($locations[$i].location)"    
@@ -156,10 +255,21 @@ if ("Y", "y" -contains $continueInput) {
         New-AzResourceGroup -Name $resourceGroupName -Location $targetLocation
     }
 
+    $jobSchedules = Get-AzAutomationScheduledRunbook -ResourceGroupName $resourceGroupName -AutomationAccountName $automationAccountName -ErrorAction SilentlyContinue
+    if ($jobSchedules.Count -gt 0)
+    {
+        Write-Host "Unregistering previous runbook schedules associations from $automationAccountName..." -ForegroundColor Green
+        foreach ($jobSchedule in $jobSchedules)
+        {
+            Unregister-AzAutomationScheduledRunbook -ResourceGroupName $resourceGroupName -AutomationAccountName $automationAccountName `
+                -JobScheduleId $jobSchedule.JobScheduleId -Force
+        }    
+    }
+
     Write-Host "Deploying Azure Optimization Engine resources..." -ForegroundColor Green
     New-AzResourceGroupDeployment -TemplateUri $TemplateUri -ResourceGroupName $resourceGroupName -Name $deploymentName `
         -projectName $namePrefix -projectLocation $targetlocation -logAnalyticsReuse $logAnalyticsReuse `
-        -sqlAdminLogin $sqlAdmin -sqlAdminPassword $sqlPass
+        -sqlAdminLogin $sqlAdmin -sqlAdminPassword $sqlPass -artifactsLocationSasToken $ArtifactsSasToken
     
     $myPublicIp = (Invoke-WebRequest -uri "http://ifconfig.me/ip").Content
 
@@ -188,14 +298,39 @@ if ("Y", "y" -contains $continueInput) {
     
     Write-Host "Checking Azure Automation Run As account..." -ForegroundColor Green
 
-    $runAsConnection = Get-AzAutomationConnection -ResourceGroupName $resourceGroupName -AutomationAccountName $automationAccountName -Name "AzureRunAsConnection" -ErrorAction SilentlyContinue
+    $CertificateAssetName = "AzureRunAsCertificate"
+    $ConnectionAssetName = "AzureRunAsConnection"
+    $ConnectionTypeName = "AzureServicePrincipal"
+    $SelfSignedCertNoOfMonthsUntilExpired = 12
+
+    $runAsConnection = Get-AzAutomationConnection -ResourceGroupName $resourceGroupName -AutomationAccountName $automationAccountName -Name $ConnectionAssetName -ErrorAction SilentlyContinue
     
     if ($null -eq $runAsConnection) {
 
         $runasAppName = "$automationAccountName-runasaccount"
         $certPass = Read-Host "Please, input the Run As certificate password" -AsSecureString   
-        .\New-RunAsAccount.ps1 -ResourceGroup $resourceGroupName -AutomationAccountName $automationAccountName -SubscriptionId $subscriptionId `
-            -ApplicationDisplayName $runasAppName -SelfSignedCertPlainPassword $certPass -CreateClassicRunAsAccount $false
+
+        $CertificateName = $automationAccountName + $CertificateAssetName
+        $PfxCertPathForRunAsAccount = Join-Path $env:TEMP ($CertificateName + ".pfx")
+        $PfxCertPlainPasswordForRunAsAccount = $certPass
+        $CerCertPathForRunAsAccount = Join-Path $env:TEMP ($CertificateName + ".cer")
+
+        try {
+            CreateSelfSignedCertificate $CertificateName $PfxCertPlainPasswordForRunAsAccount $PfxCertPathForRunAsAccount $CerCertPathForRunAsAccount $SelfSignedCertNoOfMonthsUntilExpired   
+        }
+        catch {
+            Write-Host "Failed to create self-signed certificate. Please, run this script in an elevated prompt." -ForegroundColor Red
+            throw "Terminating due to lack of administrative privileges."
+        }
+
+        $PfxCert = New-Object -TypeName System.Security.Cryptography.X509Certificates.X509Certificate2 -ArgumentList @($PfxCertPathForRunAsAccount, $PfxCertPlainPasswordForRunAsAccount)
+        $ApplicationId = CreateServicePrincipal $PfxCert $runasAppName
+        
+        CreateAutomationCertificateAsset $ResourceGroup $automationAccountName $CertificateAssetName $PfxCertPathForRunAsAccount $PfxCertPlainPasswordForRunAsAccount $true
+        
+        $ConnectionFieldValues = @{"ApplicationId" = $ApplicationId; "TenantId" = $ctx.Subscription.TenantId; "CertificateThumbprint" = $PfxCert.Thumbprint; "SubscriptionId" = $ctx.Subscription.Id}
+
+        CreateAutomationConnectionAsset $resourceGroupName $automationAccountName $ConnectionAssetName $ConnectionTypeName $ConnectionFieldValues
     }
 
     Write-Host "Deploying SQL Database model..." -ForegroundColor Green
