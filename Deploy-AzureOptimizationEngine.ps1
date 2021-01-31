e1bd1999a8ddc5656bc9513246be3882e0d53ae8
Helder Pinto <hepint@microsoft.com>
2021-01-31 21:31:05 +0000

    Fixed original base time

--- Deploy-AzureOptimizationEngine.ps1
+++ Deploy-AzureOptimizationEngine.ps1
@@ -413,7 +413,7 @@ if ("Y", "y" -contains $continueInput) {
     $schedules = Get-AzAutomationSchedule -ResourceGroupName $resourceGroupName -AutomationAccountName $automationAccountName -ErrorAction SilentlyContinue
     if ($schedules.Count -gt 0) {
         $upgradingSchedules = $true
-        $originalBaseTime = ($schedules | Where-Object { $_.Name.EndsWith("Weekly") } | Sort-Object -Property StartTime | Select-Object -First 1).StartTime.AddHours(-2).DateTime
+        $originalBaseTime = ($schedules | Where-Object { $_.Name.EndsWith("Weekly") } | Sort-Object -Property StartTime | Select-Object -First 1).StartTime.AddHours(-1.75).DateTime
         $now = (Get-Date).ToUniversalTime()
         $diff = $now - $originalBaseTime
         $nextWeekDays = [Math]::Ceiling($diff.TotalDays / 7) * 7
