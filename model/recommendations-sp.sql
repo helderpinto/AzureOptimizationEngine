566f31df68bc5e98f2d2b310d26a41fdef38c14c
Helder Pinto <hepint@microsoft.com>
2021-02-07 19:03:41 +0000

    Fixing SP deployment issues

--- model/recommendations-sp.sql
+++ model/recommendations-sp.sql
@@ -1,13 +1,14 @@
-IF OBJECT_ID ( N'[dbo].[GetRecommendations]', 'P' ) IS NOT NULL   
-    DROP PROCEDURE [dbo].[GetRecommendations];  
-GO  
+IF OBJECT_ID ( N'[dbo].[GetRecommendations]', 'P' ) IS NOT NULL
+    DROP PROCEDURE [dbo].[GetRecommendations]
+GO
 
 CREATE PROCEDURE dbo.GetRecommendations   
 AS   
+BEGIN
     SET NOCOUNT ON;  
     SELECT * FROM [dbo].[Recommendations] R
     WHERE GeneratedDate > GETDATE()-365 AND NOT EXISTS (
         SELECT * FROM [dbo].[Filters]
         WHERE FilterType IN ('Snooze', 'Dismiss') AND IsEnabled = 1 AND FilterEndDate > GETDATE() AND RecommendationSubTypeId = R.RecommendationSubTypeId AND InstanceId = R.InstanceId 
     )  
-GO  
+END
