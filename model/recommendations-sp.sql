f1e5797bef3b705a500b7899e0bbb4b868b3b434
Helder Pinto <hepint@microsoft.com>
2021-02-07 19:17:21 +0000

    Fixed SP deployment

--- model/recommendations-sp.sql
+++ model/recommendations-sp.sql
@@ -1,14 +1,14 @@
 IF OBJECT_ID ( N'[dbo].[GetRecommendations]', 'P' ) IS NOT NULL
-    DROP PROCEDURE [dbo].[GetRecommendations]
-GO
-
-CREATE PROCEDURE dbo.GetRecommendations   
-AS   
 BEGIN
-    SET NOCOUNT ON;  
-    SELECT * FROM [dbo].[Recommendations] R
-    WHERE GeneratedDate > GETDATE()-365 AND NOT EXISTS (
-        SELECT * FROM [dbo].[Filters]
-        WHERE FilterType IN ('Snooze', 'Dismiss') AND IsEnabled = 1 AND FilterEndDate > GETDATE() AND RecommendationSubTypeId = R.RecommendationSubTypeId AND InstanceId = R.InstanceId 
-    )  
+    DROP PROCEDURE dbo.GetRecommendations
+    EXEC('CREATE PROCEDURE dbo.GetRecommendations   
+        AS BEGIN
+            SET NOCOUNT ON;  
+            SELECT * FROM [dbo].[Recommendations] R
+            WHERE GeneratedDate > GETDATE()-365 AND NOT EXISTS (
+                SELECT * FROM [dbo].[Filters]
+                WHERE FilterType IN (''Snooze'', ''Dismiss'') AND IsEnabled = 1 AND FilterEndDate > GETDATE() AND RecommendationSubTypeId = R.RecommendationSubTypeId AND InstanceId = R.InstanceId 
+            )  
+        END
+    ')
 END
