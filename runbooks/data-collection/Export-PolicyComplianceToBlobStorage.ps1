919908c5d4987875bc34b852183cbf777aebd63e
Helder Pinto <hepint@microsoft.com>
2021-05-27 18:23:00 +0100

    Policy states exports and draft networking optimization

---
+++ runbooks/data-collection/Export-PolicyComplianceToBlobStorage.ps1
@@ -0,0 +1,238 @@
+param(
+    [Parameter(Mandatory = $false)]
+    [string] $TargetSubscription = $null,
+
+    [Parameter(Mandatory = $false)]
+    [string] $externalCloudEnvironment = "",
+
+    [Parameter(Mandatory = $false)]
+    [string] $externalTenantId = "",
+
+    [Parameter(Mandatory = $false)]
+    [string] $externalCredentialName = ""
+)
+
+$ErrorActionPreference = "Stop"
+
+$cloudEnvironment = Get-AutomationVariable -Name "AzureOptimization_CloudEnvironment" -ErrorAction SilentlyContinue # AzureCloud|AzureChinaCloud
+if ([string]::IsNullOrEmpty($cloudEnvironment))
+{
+    $cloudEnvironment = "AzureCloud"
+}
+$referenceRegion = Get-AutomationVariable -Name "AzureOptimization_ReferenceRegion" -ErrorAction SilentlyContinue # e.g., westeurope
+if ([string]::IsNullOrEmpty($referenceRegion))
+{
+    $referenceRegion = "westeurope"
+}
+$authenticationOption = Get-AutomationVariable -Name  "AzureOptimization_AuthenticationOption" -ErrorAction SilentlyContinue # RunAsAccount|ManagedIdentity
+if ([string]::IsNullOrEmpty($authenticationOption))
+{
+    $authenticationOption = "RunAsAccount"
+}
+
+# get ARG exports sink (storage account) details
+$storageAccountSink = Get-AutomationVariable -Name  "AzureOptimization_StorageSink"
+$storageAccountSinkRG = Get-AutomationVariable -Name  "AzureOptimization_StorageSinkRG"
+$storageAccountSinkSubscriptionId = Get-AutomationVariable -Name  "AzureOptimization_StorageSinkSubId"
+$storageAccountSinkContainer = Get-AutomationVariable -Name  "AzureOptimization_PolicyStatesContainer" -ErrorAction SilentlyContinue
+if ([string]::IsNullOrEmpty($storageAccountSinkContainer))
+{
+    $storageAccountSinkContainer = "policystateexports"
+}
+
+if (-not([string]::IsNullOrEmpty($externalCredentialName)))
+{
+    $externalCredential = Get-AutomationPSCredential -Name $externalCredentialName
+}
+
+$ARGPageSize = 1000
+
+Write-Output "Logging in to Azure with $authenticationOption..."
+
+switch ($authenticationOption) {
+    "RunAsAccount" { 
+        $ArmConn = Get-AutomationConnection -Name AzureRunAsConnection
+        Connect-AzAccount -ServicePrincipal -EnvironmentName $cloudEnvironment -Tenant $ArmConn.TenantID -ApplicationId $ArmConn.ApplicationID -CertificateThumbprint $ArmConn.CertificateThumbprint
+        break
+    }
+    "ManagedIdentity" { 
+        Connect-AzAccount -Identity -EnvironmentName $cloudEnvironment
+        break
+    }
+    Default {
+        $ArmConn = Get-AutomationConnection -Name AzureRunAsConnection
+        Connect-AzAccount -ServicePrincipal -EnvironmentName $cloudEnvironment -Tenant $ArmConn.TenantID -ApplicationId $ArmConn.ApplicationID -CertificateThumbprint $ArmConn.CertificateThumbprint
+        break
+    }
+}
+
+Select-AzSubscription -SubscriptionId $storageAccountSinkSubscriptionId
+$sa = Get-AzStorageAccount -ResourceGroupName $storageAccountSinkRG -Name $storageAccountSink
+
+$cloudSuffix = ""
+
+if (-not([string]::IsNullOrEmpty($externalCredentialName)))
+{
+    Connect-AzAccount -ServicePrincipal -EnvironmentName $externalCloudEnvironment -Tenant $externalTenantId -Credential $externalCredential 
+    $cloudSuffix = $externalCloudEnvironment.ToLower() + "-"
+    $cloudEnvironment = $externalCloudEnvironment   
+}
+
+$tenantId = (Get-AzContext).Tenant.Id
+
+$allpolicyStates = @()
+
+Write-Output "Getting subscriptions target $TargetSubscription"
+if (-not([string]::IsNullOrEmpty($TargetSubscription)))
+{
+    $subscriptions = $TargetSubscription
+    $subscriptionSuffix = $TargetSubscription
+}
+else
+{
+    $subscriptions = Get-AzSubscription | Where-Object { $_.State -eq "Enabled" } | ForEach-Object { "$($_.Id)"}
+    $subscriptionSuffix = $cloudSuffix + "-all-" + $tenantId
+}
+
+Write-Output "Building Policy display names..."
+
+$policyAssignments = @{}
+$policyInitiatives = @{}
+$policyDefinitions = @{}
+
+foreach ($sub in $subscriptions)
+{
+    Select-AzSubscription -SubscriptionId $sub | Out-Null
+    $assignments = Get-AzPolicyAssignment -IncludeDescendent
+    foreach ($assignment in $assignments)
+    {
+        if (-not($policyAssignments[$assignment.PolicyAssignmentId]))
+        {
+            $policyAssignments.Add($assignment.PolicyAssignmentId, $assignment.Properties.DisplayName)
+        }
+    }
+
+    $initiatives = Get-AzPolicySetDefinition
+    foreach ($initiative in $initiatives)
+    {
+        if (-not($policyInitiatives[$initiative.PolicySetDefinitionId]))
+        {
+            $policyInitiatives.Add($initiative.PolicySetDefinitionId, $initiative.Properties.DisplayName)
+        }
+    }
+
+    $definitions = Get-AzPolicyDefinition
+    foreach ($definition in $definitions)
+    {
+        if (-not($policyDefinitions[$definition.PolicyDefinitionId]))
+        {
+            $policyDefinitions.Add($definition.PolicyDefinitionId, $definition.Properties.DisplayName)
+        }
+    }
+}
+
+$policyStatesTotal = @()
+
+$resultsSoFar = 0
+
+Write-Output "Querying for Policy states"
+
+$argQuery = @"
+policyresources
+| extend effect = tostring(properties.policyDefinitionAction)
+| extend assignmentId = tolower(properties.policyAssignmentId)
+| extend definitionId = tolower(properties.policyDefinitionId)
+| extend definitionReferenceId = tolower(properties.policyDefinitionReferenceId)
+| extend initiativeId = tolower(properties.policySetDefinitionId)
+| extend complianceState = tostring(properties.complianceState)
+| extend complianceReason = tostring(properties.complianceReasonCode)
+| extend resourceId = tolower(properties.resourceId)
+| extend resourceType = tostring(properties.resourceType)
+| extend evaluatedOn = tostring(properties.timestamp)
+| where complianceState != 'Compliant' and complianceReason !contains 'ResourceNotFound'
+| summarize StatesCount = count() by id, tenantId, subscriptionId, resourceGroup, resourceId, resourceType, complianceState, complianceReason, effect, assignmentId, definitionReferenceId, definitionId, initiativeId, evaluatedOn
+| union ( policyresources
+	| extend effect = tostring(properties.policyDefinitionAction)
+	| extend assignmentId = tolower(properties.policyAssignmentId)
+	| extend definitionId = tolower(properties.policyDefinitionId)
+	| extend definitionReferenceId = tolower(properties.policyDefinitionReferenceId)
+	| extend initiativeId = tolower(properties.policySetDefinitionId)
+	| extend complianceState = tostring(properties.complianceState)
+	| extend resourceType = tostring(properties.resourceType)
+	| extend evaluatedOn = tostring(properties.timestamp)
+	| where complianceState == 'Compliant'
+	| summarize StatesCount = count() by tenantId, subscriptionId, complianceState, effect, assignmentId, definitionReferenceId, definitionId, initiativeId
+)
+| order by id asc
+"@
+
+do
+{
+    if ($resultsSoFar -eq 0)
+    {
+        $policyStates = Search-AzGraph -Query $argQuery -First $ARGPageSize -Subscription $subscriptions
+    }
+    else
+    {
+        $policyStates = Search-AzGraph -Query $argQuery -First $ARGPageSize -Skip $resultsSoFar -Subscription $subscriptions 
+    }
+    $resultsCount = $policyStates.Count
+    $resultsSoFar += $resultsCount
+    $policyStatesTotal += $policyStates
+
+} while ($resultsCount -eq $ARGPageSize)
+
+$datetime = (Get-Date).ToUniversalTime()
+$timestamp = $datetime.ToString("yyyy-MM-ddTHH:mm:00.000Z")
+$statusDate = $datetime.ToString("yyyy-MM-dd")
+
+Write-Output "Building $($policyStatesTotal.Count) policyState entries"
+
+foreach ($policyState in $policyStatesTotal)
+{
+    $resourceGroup = $null
+    if ($policyState.resourceGroup)
+    {
+        $resourceGroup = $policyState.resourceGroup.ToLower()
+    }
+
+    $logentry = New-Object PSObject -Property @{
+        Timestamp = $timestamp
+        Cloud = $cloudEnvironment
+        TenantGuid = $policyState.tenantId
+        SubscriptionGuid = $policyState.subscriptionId
+        ResourceGroupName = $resourceGroup
+        ResourceId = $policyState.resourceId
+        ResourceType = $policyState.resourceType
+        ComplianceState = $policyState.complianceState
+        ComplianceReason = $policyState.complianceReason
+        Effect = $policyState.effect
+        AssignmentId = $policyState.assignmentId
+        AssignmentName = $policyAssignments[$policyState.assignmentId]
+        InitiativeId = $policyState.initiativeId
+        InitiativeName = $policyInitiatives[$policyState.initiativeId]
+        DefinitionId = $policyState.definitionId
+        DefinitionName = $policyDefinitions[$policyState.definitionId]
+        DefinitionReferenceId = $policyState.definitionReferenceId
+        EvaluatedOn = $policyState.evaluatedOn
+        StatesCount = $policyState.StatesCount
+        StatusDate = $statusDate
+    }
+    
+    $allpolicyStates += $logentry
+}
+
+Write-Output "Uploading CSV to Storage"
+
+$today = $datetime.ToString("yyyyMMdd")
+$csvExportPath = "$today-policyStates-$subscriptionSuffix.csv"
+
+$allpolicyStates | Export-Csv -Path $csvExportPath -NoTypeInformation
+
+$csvBlobName = $csvExportPath
+
+$csvProperties = @{"ContentType" = "text/csv"};
+
+Set-AzStorageBlobContent -File $csvExportPath -Container $storageAccountSinkContainer -Properties $csvProperties -Blob $csvBlobName -Context $sa.Context -Force
+
+Write-Output "DONE"
---
+++ runbooks/recommendations/Recommend-NetworkingOptimizationsToBlobStorage.ps1
@@ -0,0 +1,376 @@
+$ErrorActionPreference = "Stop"
+
+# Collect generic and recommendation-specific variables
+
+$cloudEnvironment = Get-AutomationVariable -Name "AzureOptimization_CloudEnvironment" -ErrorAction SilentlyContinue # AzureCloud|AzureChinaCloud
+if ([string]::IsNullOrEmpty($cloudEnvironment))
+{
+    $cloudEnvironment = "AzureCloud"
+}
+$authenticationOption = Get-AutomationVariable -Name "AzureOptimization_AuthenticationOption" -ErrorAction SilentlyContinue # RunAsAccount|ManagedIdentity
+if ([string]::IsNullOrEmpty($authenticationOption))
+{
+    $authenticationOption = "RunAsAccount"
+}
+
+$workspaceId = Get-AutomationVariable -Name  "AzureOptimization_LogAnalyticsWorkspaceId"
+$workspaceName = Get-AutomationVariable -Name  "AzureOptimization_LogAnalyticsWorkspaceName"
+$workspaceRG = Get-AutomationVariable -Name  "AzureOptimization_LogAnalyticsWorkspaceRG"
+$workspaceSubscriptionId = Get-AutomationVariable -Name  "AzureOptimization_LogAnalyticsWorkspaceSubId"
+$workspaceTenantId = Get-AutomationVariable -Name  "AzureOptimization_LogAnalyticsWorkspaceTenantId"
+
+$storageAccountSink = Get-AutomationVariable -Name  "AzureOptimization_StorageSink"
+$storageAccountSinkRG = Get-AutomationVariable -Name  "AzureOptimization_StorageSinkRG"
+$storageAccountSinkSubscriptionId = Get-AutomationVariable -Name  "AzureOptimization_StorageSinkSubId"
+$storageAccountSinkContainer = Get-AutomationVariable -Name  "AzureOptimization_RecommendationsContainer" -ErrorAction SilentlyContinue 
+if ([string]::IsNullOrEmpty($storageAccountSinkContainer)) {
+    $storageAccountSinkContainer = "recommendationsexports"
+}
+
+$deploymentDate = Get-AutomationVariable -Name  "AzureOptimization_DeploymentDate" # yyyy-MM-dd format
+$deploymentDate = $deploymentDate.Replace('"', "")
+
+$lognamePrefix = Get-AutomationVariable -Name  "AzureOptimization_LogAnalyticsLogPrefix" -ErrorAction SilentlyContinue
+if ([string]::IsNullOrEmpty($lognamePrefix))
+{
+    $lognamePrefix = "AzureOptimization"
+}
+
+$sqlserver = Get-AutomationVariable -Name  "AzureOptimization_SQLServerHostname"
+$sqlserverCredential = Get-AutomationPSCredential -Name "AzureOptimization_SQLServerCredential"
+$SqlUsername = $sqlserverCredential.UserName 
+$SqlPass = $sqlserverCredential.GetNetworkCredential().Password 
+$sqldatabase = Get-AutomationVariable -Name  "AzureOptimization_SQLServerDatabase" -ErrorAction SilentlyContinue
+if ([string]::IsNullOrEmpty($sqldatabase))
+{
+    $sqldatabase = "azureoptimization"
+}
+
+$SqlTimeout = 120
+$LogAnalyticsIngestControlTable = "LogAnalyticsIngestControl"
+
+# Authenticate against Azure
+
+Write-Output "Logging in to Azure with $authenticationOption..."
+
+switch ($authenticationOption) {
+    "RunAsAccount" { 
+        $ArmConn = Get-AutomationConnection -Name AzureRunAsConnection
+        Connect-AzAccount -ServicePrincipal -EnvironmentName $cloudEnvironment -Tenant $ArmConn.TenantID -ApplicationId $ArmConn.ApplicationID -CertificateThumbprint $ArmConn.CertificateThumbprint
+        break
+    }
+    "ManagedIdentity" { 
+        Connect-AzAccount -Identity
+        break
+    }
+    Default {
+        $ArmConn = Get-AutomationConnection -Name AzureRunAsConnection
+        Connect-AzAccount -ServicePrincipal -EnvironmentName $cloudEnvironment -Tenant $ArmConn.TenantID -ApplicationId $ArmConn.ApplicationID -CertificateThumbprint $ArmConn.CertificateThumbprint
+        break
+    }
+}
+
+Write-Output "Finding tables where recommendations will be generated from..."
+
+$tries = 0
+$connectionSuccess = $false
+do {
+    $tries++
+    try {
+        $Conn = New-Object System.Data.SqlClient.SqlConnection("Server=tcp:$sqlserver,1433;Database=$sqldatabase;User ID=$SqlUsername;Password=$SqlPass;Trusted_Connection=False;Encrypt=True;Connection Timeout=$SqlTimeout;") 
+        $Conn.Open() 
+        $Cmd=new-object system.Data.SqlClient.SqlCommand
+        $Cmd.Connection = $Conn
+        $Cmd.CommandTimeout = $SqlTimeout
+        $Cmd.CommandText = "SELECT * FROM [dbo].[$LogAnalyticsIngestControlTable] WHERE CollectedType IN ('ARGLoadBalancer','AzureConsumption','ARGResourceContainers')"
+    
+        $sqlAdapter = New-Object System.Data.SqlClient.SqlDataAdapter
+        $sqlAdapter.SelectCommand = $Cmd
+        $controlRows = New-Object System.Data.DataTable
+        $sqlAdapter.Fill($controlRows) | Out-Null            
+        $connectionSuccess = $true
+    }
+    catch {
+        Write-Output "Failed to contact SQL at try $tries."
+        Write-Output $Error[0]
+        Start-Sleep -Seconds ($tries * 20)
+    }    
+} while (-not($connectionSuccess) -and $tries -lt 3)
+
+if (-not($connectionSuccess))
+{
+    throw "Could not establish connection to SQL."
+}
+
+$lbsTableName = $lognamePrefix + ($controlRows | Where-Object { $_.CollectedType -eq 'ARGLoadBalancer' }).LogAnalyticsSuffix + "_CL"
+$consumptionTableName = $lognamePrefix + ($controlRows | Where-Object { $_.CollectedType -eq 'AzureConsumption' }).LogAnalyticsSuffix + "_CL"
+$subscriptionsTableName = $lognamePrefix + ($controlRows | Where-Object { $_.CollectedType -eq 'ARGResourceContainers' }).LogAnalyticsSuffix + "_CL"
+
+Write-Output "Will run query against tables $lbsTableName, $subscriptionsTableName and $consumptionTableName"
+
+$Conn.Close()    
+$Conn.Dispose()            
+
+$recommendationSearchTimeSpan = 30 + $consumptionOffsetDaysStart
+
+# Grab a context reference to the Storage Account where the recommendations file will be stored
+
+Select-AzSubscription -SubscriptionId $storageAccountSinkSubscriptionId
+$sa = Get-AzStorageAccount -ResourceGroupName $storageAccountSinkRG -Name $storageAccountSink
+
+if ($workspaceSubscriptionId -ne $storageAccountSinkSubscriptionId)
+{
+    Select-AzSubscription -SubscriptionId $workspaceSubscriptionId
+}
+
+# Execute the Cost recommendation query against Log Analytics
+
+<#
+
+Orphaned NICs
+
+AzureOptimizationNICsV1_CL
+| where isempty(OwnerVMId_s) and isempty(OwnerPEId_s)
+
+AzureOptimizationVNetsV1_CL
+| where SubnetName_s !in ('GatewaySubnet','gatewaysubnet')
+| extend FreeIPs = toint(SubnetTotalPrefixIPs_s) - toint(SubnetUsedIPs_s)
+| extend UsedIPPercentage = (todouble(SubnetUsedIPs_s) / todouble(SubnetTotalPrefixIPs_s)) * 100
+| where UsedIPPercentage > 80
+
+AzureOptimizationVNetsV1_CL
+| where toint(SubnetUsedIPs_s) == 0
+
+#>
+
+
+$baseQuery = @"
+    let interval = 30d;
+    let etime = todatetime(toscalar($consumptionTableName | summarize max(UsageDate_t))); 
+    let stime = etime-interval; 
+    $lbsTableName
+    | where TimeGenerated > ago(1d)
+    | where SkuName_s == 'Standard'
+    | where (toint(BackendPoolsCount_s) == 0 or BackendIPCount_s == 0 or isempty(BackendIPCount_s)) and toint(InboundNatPoolsCount_s) == 0
+    | where toint(LbRulesCount_s) != 0 or toint(InboundNatRulesCount_s) != 0 or toint(OutboundRulesCount_s) != 0
+    | distinct InstanceName_s, InstanceId_s, SubscriptionGuid_g, TenantGuid_g, ResourceGroupName_s, SkuName_s, Tags_s, Cloud_s 
+    | join kind=leftouter (
+        $consumptionTableName
+        | where UsageDate_t between (stime..etime)
+    ) on InstanceId_s
+    | summarize Last30DaysCost=sum(todouble(Cost_s)) by InstanceName_s, InstanceId_s, SubscriptionGuid_g, TenantGuid_g, ResourceGroupName_s, SkuName_s, Tags_s, Cloud_s    
+    | join kind=leftouter ( 
+        $subscriptionsTableName 
+        | where ContainerType_s =~ 'microsoft.resources/subscriptions' 
+        | project SubscriptionGuid_g, SubscriptionName = ContainerName_s 
+    ) on SubscriptionGuid_g
+"@
+
+try
+{
+    $queryResults = Invoke-AzOperationalInsightsQuery -WorkspaceId $workspaceId -Query $baseQuery -Timespan (New-TimeSpan -Days $recommendationSearchTimeSpan) -Wait 600 -IncludeStatistics
+    if ($queryResults)
+    {
+        $results = [System.Linq.Enumerable]::ToArray($queryResults.Results)        
+    }
+}
+catch
+{
+    Write-Warning -Message "Query failed. Debug the following query in the AOE Log Analytics workspace: $baseQuery"    
+}
+
+Write-Output "Costs query finished with $($results.Count) results."
+
+Write-Output "Query statistics: $($queryResults.Statistics.query)"
+
+# Build the recommendations objects
+
+$recommendations = @()
+$datetime = (get-date).ToUniversalTime()
+$timestamp = $datetime.ToString("yyyy-MM-ddTHH:mm:00.000Z")
+
+foreach ($result in $results)
+{
+    $queryInstanceId = $result.InstanceId_s
+    $queryText = @"
+    $lbsTableName
+    | where InstanceId_s == '$queryInstanceId'
+    | where SkuName_s == 'Standard'
+    | where (toint(BackendPoolsCount_s) == 0 or BackendIPCount_s == 0 or isempty(BackendIPCount_s)) and toint(InboundNatPoolsCount_s) == 0
+    | where toint(LbRulesCount_s) != 0 or toint(InboundNatRulesCount_s) != 0 or toint(OutboundRulesCount_s) != 0
+    | distinct InstanceId_s, InstanceName_s, SkuName_s, TimeGenerated
+    | summarize FirstUnusedDate = min(TimeGenerated) by InstanceId_s, InstanceName_s, SkuName_s
+    | join kind=inner (
+        $consumptionTableName
+    ) on InstanceId_s
+    | where UsageDate_t > FirstUnusedDate
+    | summarize CostsSinceUnused = sum(todouble(Cost_s)) by InstanceName_s, FirstUnusedDate, SkuName_s
+"@
+    $encodedQuery = [System.Uri]::EscapeDataString($queryText)
+    $detailsQueryStart = $deploymentDate
+    $detailsQueryEnd = $datetime.AddDays(8).ToString("yyyy-MM-dd")
+    $detailsURL = "https://portal.azure.com#@$workspaceTenantId/blade/Microsoft_Azure_Monitoring_Logs/LogsBlade/resourceId/%2Fsubscriptions%2F$workspaceSubscriptionId%2Fresourcegroups%2F$workspaceRG%2Fproviders%2Fmicrosoft.operationalinsights%2Fworkspaces%2F$workspaceName/source/LogsBlade.AnalyticsShareLinkToQuery/query/$encodedQuery/timespan/$($detailsQueryStart)T00%3A00%3A00.000Z%2F$($detailsQueryEnd)T00%3A00%3A00.000Z"
+
+    $additionalInfoDictionary = @{}
+
+    $additionalInfoDictionary["currentSku"] = $result.SkuName_s
+    $additionalInfoDictionary["CostsAmount"] = [double] $result.Last30DaysCost 
+    $additionalInfoDictionary["savingsAmount"] = [double] $result.Last30DaysCost 
+
+    $fitScore = 5
+
+    $tags = @{}
+
+    if (-not([string]::IsNullOrEmpty($result.Tags_s)))
+    {
+        $tagPairs = $result.Tags_s.Substring(2, $result.Tags_s.Length - 3).Split(';')
+        foreach ($tagPairString in $tagPairs)
+        {
+            $tagPair = $tagPairString.Split('=')
+            if (-not([string]::IsNullOrEmpty($tagPair[0])) -and -not([string]::IsNullOrEmpty($tagPair[1])))
+            {
+                $tagName = $tagPair[0].Trim()
+                $tagValue = $tagPair[1].Trim()
+                $tags[$tagName] = $tagValue    
+            }
+        }
+    }
+
+    $recommendation = New-Object PSObject -Property @{
+        Timestamp = $timestamp
+        Cloud = $result.Cloud_s
+        Category = "Cost"
+        ImpactedArea = "Microsoft.Network/loadBalancers"
+        Impact = "Medium"
+        RecommendationType = "Saving"
+        RecommendationSubType = "UnusedStandardLoadBalancers"
+        RecommendationSubTypeId = "f1ed3bb2-3cb5-41e6-ba38-7001d5ff87f5"
+        RecommendationDescription = "Standard Load Balancers with rules defined and without a backend pool incur in unnecessary costs"
+        RecommendationAction = "Delete the Load Balancer"
+        InstanceId = $result.InstanceId_s
+        InstanceName = $result.InstanceName_s
+        AdditionalInfo = $additionalInfoDictionary
+        ResourceGroup = $result.ResourceGroupName_s
+        SubscriptionGuid = $result.SubscriptionGuid_g
+        SubscriptionName = $result.SubscriptionName
+        TenantGuid = $result.TenantGuid_g
+        FitScore = $fitScore
+        Tags = $tags
+        DetailsURL = $detailsURL
+    }
+
+    $recommendations += $recommendation
+}
+
+# Export the recommendations as JSON to blob storage
+
+$fileDate = $datetime.ToString("yyyy-MM-dd")
+$jsonExportPath = "unusedstdloadbalancers-$fileDate.json"
+$recommendations | ConvertTo-Json | Out-File $jsonExportPath
+
+$jsonBlobName = $jsonExportPath
+$jsonProperties = @{"ContentType" = "application/json"};
+Set-AzStorageBlobContent -File $jsonExportPath -Container $storageAccountSinkContainer -Properties $jsonProperties -Blob $jsonBlobName -Context $sa.Context -Force
+
+
+# Execute the Operational Excellence recommendation query against Log Analytics
+
+$baseQuery = @"
+    $lbsTableName
+    | where TimeGenerated > ago(1d)
+    | where (toint(BackendPoolsCount_s) == 0 or BackendIPCount_s == 0 or isempty(BackendIPCount_s)) and toint(InboundNatPoolsCount_s) == 0
+    | distinct InstanceName_s, InstanceId_s, SubscriptionGuid_g, TenantGuid_g, ResourceGroupName_s, SkuName_s, Tags_s, Cloud_s 
+    | join kind=leftouter ( 
+        $subscriptionsTableName
+        | where TimeGenerated > ago(1d) 
+        | where ContainerType_s =~ 'microsoft.resources/subscriptions' 
+        | project SubscriptionGuid_g, SubscriptionName = ContainerName_s 
+    ) on SubscriptionGuid_g
+"@
+
+try 
+{
+    $queryResults = Invoke-AzOperationalInsightsQuery -WorkspaceId $workspaceId -Query $baseQuery -Timespan (New-TimeSpan -Days 2) -Wait 600 -IncludeStatistics
+    if ($queryResults)
+    {
+        $results = [System.Linq.Enumerable]::ToArray($queryResults.Results)
+    }
+}
+catch
+{
+    Write-Warning -Message "Query failed. Debug the following query in the AOE Log Analytics workspace: $baseQuery"    
+}
+
+Write-Output "Operational Excellence query finished with $($results.Count) results."
+
+Write-Output "Query statistics: $($queryResults.Statistics.query)"
+
+# Build the recommendations objects
+
+$recommendations = @()
+$datetime = (get-date).ToUniversalTime()
+$timestamp = $datetime.ToString("yyyy-MM-ddTHH:mm:00.000Z")
+
+foreach ($result in $results)
+{
+    $queryInstanceId = $result.InstanceId_s
+    $detailsURL = "https://portal.azure.com/#@$workspaceTenantId/resource/$queryInstanceId/overview"
+
+    $additionalInfoDictionary = @{}
+
+    $additionalInfoDictionary["currentSku"] = $result.SkuName_s
+
+    $fitScore = 5
+
+    $tags = @{}
+
+    if (-not([string]::IsNullOrEmpty($result.Tags_s)))
+    {
+        $tagPairs = $result.Tags_s.Substring(2, $result.Tags_s.Length - 3).Split(';')
+        foreach ($tagPairString in $tagPairs)
+        {
+            $tagPair = $tagPairString.Split('=')
+            if (-not([string]::IsNullOrEmpty($tagPair[0])) -and -not([string]::IsNullOrEmpty($tagPair[1])))
+            {
+                $tagName = $tagPair[0].Trim()
+                $tagValue = $tagPair[1].Trim()
+                $tags[$tagName] = $tagValue    
+            }
+        }
+    }
+
+    $recommendation = New-Object PSObject -Property @{
+        Timestamp = $timestamp
+        Cloud = $result.Cloud_s
+        Category = "OperationalExcellence"
+        ImpactedArea = "Microsoft.Network/loadBalancers"
+        Impact = "Medium"
+        RecommendationType = "BestPractices"
+        RecommendationSubType = "UnusedLoadBalancers"
+        RecommendationSubTypeId = "48619512-f4e6-4241-9c85-16f7c987950c"
+        RecommendationDescription = "Load Balancers without a backend pool are useless"
+        RecommendationAction = "Delete the Load Balancer"
+        InstanceId = $result.InstanceId_s
+        InstanceName = $result.InstanceName_s
+        AdditionalInfo = $additionalInfoDictionary
+        ResourceGroup = $result.ResourceGroupName_s
+        SubscriptionGuid = $result.SubscriptionGuid_g
+        SubscriptionName = $result.SubscriptionName
+        TenantGuid = $result.TenantGuid_g
+        FitScore = $fitScore
+        Tags = $tags
+        DetailsURL = $detailsURL
+    }
+
+    $recommendations += $recommendation
+}
+
+# Export the recommendations as JSON to blob storage
+
+$fileDate = $datetime.ToString("yyyy-MM-dd")
+$jsonExportPath = "unusedloadbalancers-$fileDate.json"
+$recommendations | ConvertTo-Json | Out-File $jsonExportPath
+
+$jsonBlobName = $jsonExportPath
+$jsonProperties = @{"ContentType" = "application/json"};
+Set-AzStorageBlobContent -File $jsonExportPath -Container $storageAccountSinkContainer -Properties $jsonProperties -Blob $jsonBlobName -Context $sa.Context -Force
