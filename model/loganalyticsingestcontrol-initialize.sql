93618be5fea3529957d0fdb49957d2e93b7cd368
Helder Pinto <hepint@microsoft.com>
2022-06-10 15:27:39 +0100

    Added Policy States control state

--- model/loganalyticsingestcontrol-initialize.sql
+++ model/loganalyticsingestcontrol-initialize.sql
@@ -111,3 +111,9 @@ BEGIN
     INSERT INTO [dbo].[LogAnalyticsIngestControl] 
     VALUES ('azmonitorexports', '1901-01-01T00:00:00Z', -1, 'MonitorMetricsV1', 'MonitorMetrics')
 END
+
+IF NOT EXISTS (SELECT * FROM [dbo].[LogAnalyticsIngestControl] WHERE StorageContainerName = 'policystateexports')
+BEGIN
+    INSERT INTO [dbo].[LogAnalyticsIngestControl] 
+    VALUES ('policystateexports', '1901-01-01T00:00:00Z', -1, 'PolicyStatesV1', 'PolicyStates')
+END
