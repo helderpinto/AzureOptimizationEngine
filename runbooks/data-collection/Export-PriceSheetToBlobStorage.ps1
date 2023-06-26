f6146aac2ffc7d409ac31c44b8061c67e8f2d264
Hélder Pinto <hepint@microsoft.com>
2023-06-26 22:59:10 +0100

    Added support for MCA pricesheets

--- runbooks/data-collection/Export-PriceSheetToBlobStorage.ps1
+++ runbooks/data-collection/Export-PriceSheetToBlobStorage.ps1
@@ -3,6 +3,9 @@ param(
     [string] $BillingAccountID,
 
     [Parameter(Mandatory = $false)]
+    [string] $BillingProfileID,
+
+    [Parameter(Mandatory = $false)]
     [string] $externalCloudEnvironment = "",
 
     [Parameter(Mandatory = $false)]
@@ -77,6 +80,7 @@ $consumptionOffsetDays = [int] (Get-AutomationVariable -Name  "AzureOptimization
 $meterCategoriesVar = Get-AutomationVariable -Name "AzureOptimization_PriceSheetMeterCategories" -ErrorAction SilentlyContinue
 $meterRegionsVar = Get-AutomationVariable -Name "AzureOptimization_PriceSheetMeterRegions" -ErrorAction SilentlyContinue
 $BillingAccountIDVar = Get-AutomationVariable -Name  "AzureOptimization_BillingAccountID" -ErrorAction SilentlyContinue
+$BillingProfileIDVar = Get-AutomationVariable -Name  "AzureOptimization_BillingProfileID" -ErrorAction SilentlyContinue
 
 Write-Output "Logging in to Azure with $authenticationOption..."
 
@@ -106,9 +110,30 @@ if (-not([string]::IsNullOrEmpty($BillingAccountIDVar)))
     $BillingAccountID = $BillingAccountIDVar
 }
 
+if (-not([string]::IsNullOrEmpty($BillingProfileIDVar)))
+{
+    $BillingProfileID = $BillingProfileIDVar
+}
+
+$mcaBillingAccountIdRegex = "([A-Za-z0-9]+(-[A-Za-z0-9]+)+):([A-Za-z0-9]+(-[A-Za-z0-9]+)+)_[0-9]{4}-[0-9]{2}-[0-9]{2}"
+$mcaBillingProfileIdRegex = "([A-Za-z0-9]+(-[A-Za-z0-9]+)+)"
+
 if ([string]::IsNullOrEmpty($BillingAccountID))
 {
     throw "Billing Account ID undefined. Use either the AzureOptimization_BillingAccountID variable or the BillingAccountID parameter"
+}
+else {
+    if ($BillingAccountID -match $mcaBillingAccountIdRegex)
+    {
+        if ([string]::IsNullOrEmpty($BillingProfileID))
+        {
+            throw "Billing Profile ID undefined for MCA. Use either the AzureOptimization_BillingProfileID variable or the BillingProfileID parameter"
+        }
+        if (-not($BillingProfileID -match $mcaBillingProfileIdRegex))
+        {
+            throw "Billing Profile ID does not follow pattern for MCA: ([A-Za-z0-9]+(-[A-Za-z0-9]+)+)"
+        }
+    }
 }
 
 if (-not([string]::IsNullOrEmpty($meterCategoriesVar)))
@@ -134,13 +159,138 @@ if (-not([string]::IsNullOrEmpty($meterRegions)))
     $meterRegionFilters = $meterRegions.Split(',')
 }
 
+function Generate-Pricesheet {
+    param (        
+        [string] $InputCSVPath,
+        [string] $OutputCSVPath,
+        [string] $HeaderLine
+    )
+
+    # header normalization between MCA and EA
+    $headerConversion = @{
+        'Meter ID' = "MeterID";
+        meterId = "MeterID";
+        'Meter name' = "MeterName";
+        meterName = "MeterName";
+        'Meter category' = "MeterCategory";
+        meterCategory = "MeterCategory";
+        'Meter sub-category' = "MeterSubCategory";
+        meterSubCategory = "MeterSubCategory";
+        'Meter region' = "MeterRegion";
+        meterRegion = "MeterRegion";
+        'Unit of measure' = "UnitOfMeasure";
+        unitOfMeasure = "UnitOfMeasure";
+        'Part number' = "PartNumber";
+        'Unit price' = "UnitPrice";
+        unitPrice = "UnitPrice";
+        'Currency code' = "CurrencyCode";
+        currency = "CurrencyCode";
+        'Included quantity' = "IncludedQuantity";
+        includedQuantity = "IncludedQuantity";
+        'Offer Id' = "OfferId";
+        Term = "Term";
+        'Price type' = "PriceType";
+        priceType = "PriceType"
+    }
+
+    $r = [IO.File]::OpenText($InputCSVPath)
+    $w = [System.IO.StreamWriter]::new($OutputCSVPath)
+    $lineCounter = 0
+    while ($r.Peek() -ge 0) {
+        $line = $r.ReadLine()
+        $lineCounter++
+        if ($lineCounter -eq $HeaderLine)
+        {
+            $headers = $line.Split(",")
+
+            for ($i = 0; $i -lt $headers.Length; $i++)
+            {
+                $header = $headers[$i]
+                if ($headerConversion.ContainsKey($header))
+                {
+                    $headers[$i] = $headerConversion[$header]
+                }
+            }
+
+            $line = $headers -join ","
+
+            if (-not($line -match "SubCategory"))
+            {
+                throw "Pricesheet format has changed at line $HeaderLine - $line"
+            }
+
+            Write-Output "New headers: $line"
+
+            $w.WriteLine($line)
+        }
+        else
+        {
+            if ($lineCounter -gt $HeaderLine)
+            {
+                $categoryWriteLine = $categoryWriteLineDefault
+                $regionWriteLine = $regionWriteLineDefault
+
+                foreach ($meterCategory in $meterCategoryFilters)
+                {
+                    if ($line -match ",$meterCategory,")
+                    {
+                        $categoryWriteLine = $true
+                        break
+                    }
+                }    
+
+                foreach ($meterRegion in $meterRegionFilters)
+                {
+                    if ($line -match ",$meterRegion,")
+                    {
+                        $regionWriteLine = $true
+                        break
+                    }
+                }    
+
+                if ($categoryWriteLine -eq $true -and $regionWriteLine -eq $true)
+                {
+                    $w.WriteLine($line)
+                }
+            }
+        }
+    }
+    $r.Dispose()
+    $w.Close()
+
+    $csvBlobName = [System.IO.Path]::GetFileName($OutputCSVPath)
+    $csvProperties = @{"ContentType" = "text/csv"};
+    Set-AzStorageBlobContent -File $OutputCSVPath -Container $storageAccountSinkContainer -Properties $csvProperties -Blob $csvBlobName -Context $sa.Context -Force
+        
+    $now = (Get-Date).ToUniversalTime().ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fff'Z'")
+    Write-Output "[$now] Uploaded $csvBlobName to Blob Storage..."
+
+    Remove-Item -Path $InputCSVPath -Force
+
+    $now = (Get-Date).ToUniversalTime().ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fff'Z'")
+    Write-Output "[$now] Removed $InputCSVPath from local disk..."                    
+
+    Remove-Item -Path $OutputCSVPath -Force
+
+    $now = (Get-Date).ToUniversalTime().ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fff'Z'")
+    Write-Output "[$now] Removed $OutputCSVPath from local disk..."                    
+}
+
 Write-Output "Starting pricesheet export process for $billingPeriod billing period for Billing Account $BillingAccountID..."
 
 $MaxTries = 30 # The typical Retry-After is set to 20 seconds. We'll give 10 minutes overall to download the pricesheet report
 
-$PriceSheetApiPath = "/providers/Microsoft.Billing/billingAccounts/$BillingAccountID/billingPeriods/$billingPeriod/providers/Microsoft.Consumption/pricesheets/download?api-version=2022-06-01&ln=en"
+if ($BillingAccountID -match $mcaBillingAccountIdRegex)
+{
+    $PriceSheetApiPath = "/providers/Microsoft.Billing/billingAccounts/$BillingAccountID/billingProfiles/$BillingProfileID/providers/Microsoft.CostManagement/pricesheets/default/download?api-version=2023-03-01&format=csv"
+    $result = Invoke-AzRestMethod -Path $PriceSheetApiPath -Method POST
+}
+else
+{
+    $PriceSheetApiPath = "/providers/Microsoft.Billing/billingAccounts/$BillingAccountID/billingPeriods/$billingPeriod/providers/Microsoft.Consumption/pricesheets/download?api-version=2022-06-01&ln=en"
+    $result = Invoke-AzRestMethod -Path $PriceSheetApiPath -Method GET
+}
 
-$result = Invoke-AzRestMethod -Path $PriceSheetApiPath -Method GET
 $requestResultPath = $result.Headers.Location.PathAndQuery
 if ($result.StatusCode -in (200,202))
 {
@@ -167,18 +317,6 @@ if ($result.StatusCode -in (200,202))
 
         if ($downloadResult.StatusCode -eq 200)
         {
-
-            Write-Output "Export is ready. Proceeding with CSV download..."
-
-            $downloadUrl = ($downloadResult.Content | ConvertFrom-Json).properties.downloadUrl
-
-            $csvExportPath = "$env:TEMP\pricesheet-$billingPeriod-$BillingAccountID.csv"
-            $finalCsvExportPath = "$env:TEMP\pricesheet-$billingPeriod-$BillingAccountID$($meterCategories.Replace(',',''))$($meterRegions.Replace(',',''))-$exportDate-final.csv"
-
-            Invoke-WebRequest -Uri $downloadUrl -OutFile $csvExportPath
-
-            Write-Output "Blob downloaded to $csvExportPath successfully."
-
             Write-Output "Filtering data with meter categories $meterCategories and meter regions $meterRegions to $finalCsvExportPath..."
 
             $categoryWriteLineDefault = $true
@@ -194,77 +332,38 @@ if ($result.StatusCode -in (200,202))
 
             Write-Output "Defaulting to meter categories writes $($categoryWriteLineDefault) and meter regions writes $($regionWriteLineDefault)..."
 
-            $r = [IO.File]::OpenText($csvExportPath)
-            $w = [System.IO.StreamWriter]::new($finalCsvExportPath)
-            $lineCounter = 0
-            while ($r.Peek() -ge 0) {
-                $line = $r.ReadLine()
-                $lineCounter++
-                if ($lineCounter -eq 3)
+            if ($BillingAccountID -match $mcaBillingAccountIdRegex)
+            {
+                Write-Output "Export is ready. Proceeding with ZIP download..."
+                $downloadUrl = ($downloadResult.Content | ConvertFrom-Json).publishedEntity.properties.downloadUrl
+                $zipExportPath = "$env:TEMP\pricesheet-$BillingProfileID-$exportDate.zip"
+                $zipExpandPath = "$env:TEMP\pricesheet"
+                Invoke-WebRequest -Uri $downloadUrl -OutFile $zipExportPath
+                Write-Output "Blob downloaded to $zipExportPath successfully."
+                Expand-Archive -LiteralPath $zipExportPath -DestinationPath $zipExpandPath -Force
+                Write-Output "Zip expanded to $zipExpandPath successfully."
+                $csvFiles = Get-ChildItem -Path $zipExpandPath -Filter *.csv -Recurse
+                foreach ($csvFile in $csvFiles)
                 {
-                    if ($line -match "Category")
-                    {
-                        $line = $line.Replace(" ", "").Replace("-","").Replace("category", "Category").Replace("name","Name").Replace("sub", "Sub").Replace("region","Region").Replace("of","Of").Replace("measure","Measure").Replace("number","Number").Replace("price","Price").Replace("code","Code").Replace("quantity","Quantity").Replace("type","Type")
-                        Write-Output "New headers: $line"
-                        $w.WriteLine($line)
-                    }
-                    else
-                    {
-                        throw "Pricesheet format has changed at line 3: $line"
-                    }
-                }
-                else
-                {
-                    if ($lineCounter -gt 3)
-                    {
-                        $categoryWriteLine = $categoryWriteLineDefault
-                        $regionWriteLine = $regionWriteLineDefault
-    
-                        foreach ($meterCategory in $meterCategoryFilters)
-                        {
-                            if ($line -match ",$meterCategory,")
-                            {
-                                $categoryWriteLine = $true
-                                break
-                            }
-                        }    
-    
-                        foreach ($meterRegion in $meterRegionFilters)
-                        {
-                            if ($line -match ",$meterRegion,")
-                            {
-                                $regionWriteLine = $true
-                                break
-                            }
-                        }    
-    
-                        if ($categoryWriteLine -eq $true -and $regionWriteLine -eq $true)
-                        {
-                            $w.WriteLine($line)
-                        }
-                    }
-                }
+                    $csvExportPath = $csvFile.FullName
+                    $finalCsvExportPath = "$env:TEMP\$($csvFile.Name)-final.csv"
+                    Generate-Pricesheet -InputCSVPath $csvExportPath -OutputCSVPath $finalCsvExportPath -HeaderLine 1
+                }         
+                Remove-Item -Path $zipExportPath -Force
+                $now = (Get-Date).ToUniversalTime().ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fff'Z'")
+                Write-Output "[$now] Removed $zipExportPath from local disk..."                           
             }
-            $r.Dispose()
-            $w.Close()
-        
-            $csvBlobName = [System.IO.Path]::GetFileName($finalCsvExportPath)
-            $csvProperties = @{"ContentType" = "text/csv"};
-            Set-AzStorageBlobContent -File $finalCsvExportPath -Container $storageAccountSinkContainer -Properties $csvProperties -Blob $csvBlobName -Context $sa.Context -Force
-                
-            $now = (Get-Date).ToUniversalTime().ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fff'Z'")
-            Write-Output "[$now] Uploaded $csvBlobName to Blob Storage..."
-
-            Remove-Item -Path $csvExportPath -Force
-        
-            $now = (Get-Date).ToUniversalTime().ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fff'Z'")
-            Write-Output "[$now] Removed $csvExportPath from local disk..."                    
-
-            Remove-Item -Path $finalCsvExportPath -Force
-        
-            $now = (Get-Date).ToUniversalTime().ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fff'Z'")
-            Write-Output "[$now] Removed $finalCsvExportPath from local disk..."                    
-
+            else
+            {
+                Write-Output "Export is ready. Proceeding with CSV download..."
+                $downloadUrl = ($downloadResult.Content | ConvertFrom-Json).properties.downloadUrl
+                $csvExportPath = "$env:TEMP\pricesheet-$billingPeriod-$BillingAccountID.csv"
+                $finalCsvExportPath = "$env:TEMP\pricesheet-$billingPeriod-$BillingAccountID$($meterCategories.Replace(',',''))$($meterRegions.Replace(',',''))-$exportDate-final.csv"
+                Invoke-WebRequest -Uri $downloadUrl -OutFile $csvExportPath
+                Write-Output "Blob downloaded to $csvExportPath successfully."
+                Generate-Pricesheet -InputCSVPath $csvExportPath -OutputCSVPath $finalCsvExportPath -HeaderLine 3
+            }
+                    
             $requestSuccess = $true
         }
         elseif ($downloadResult.StatusCode -eq 202)
